7564b38c039fa810051e6cb1118f1119930dd9d6
yesenia_v <yeseniavsqz@gmail.com>
2024-09-10 15:43:50 -0700

    Added an event listener, created a list element and added functions to make a guest list of 8 people.

--- js/script.js
+++ js/script.js
@@ -10,3 +10,41 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+
+
+addGuestButton.addEventListener("click", function() {
+    const guest = guestInput.value;  // captures the value of what is being input
+    //console.log(guest)
+   
+
+    if (guest !== "") { //if guest doesn't equal to empty string
+       addToList(guest); // calls addToList function 
+       updateGuestCount(); // 
+       clearInput(); //clears the words in the box  
+    }
+});
+
+const clearInput = function () {
+   guestInput.value = ""; 
+
+}
+
+const addToList = function (guest) {
+    const listItem = document.createElement("li")  // create a list element "li"
+    listItem.innerText = guest; // changes the text to guest variable
+    guestList.append(listItem);// adds the listItem to the guestlist
+
+};
+
+const updateGuestCount = function () {
+    const guests = document.querySelectorAll(".guest-list li") // selects all list elements inside the ul list 
+    guestCount.innerText = guests.length; //
+
+    if (guests.length === 8) { // if the length of guests equals to 8 
+        addGuestButton.classList.add("hide"); // then, add hide class to addguestbutton
+        guestInput.classList.add("hide");
+        guestInputLabel.classList.add("hide");
+        guestFull.classList.remove("hide"); // remove hide class to guestfull variable
+    }
+};
+
