f4650760c32e0336d7706a41b45a99128b2a807a
yesenia_v <yeseniavsqz@gmail.com>
2024-09-11 13:10:07 -0700

    Added functions, an array, and for ... of loop to assign dishes to guests

--- js/script.js
+++ js/script.js
@@ -10,6 +10,10 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+// button which appears when list is full
+const assignButton = document.querySelector(".assign");
+// targets the list with guest's name and dish
+const assignedItems = document.querySelector(".assigned-items")
 
 
 addGuestButton.addEventListener("click", function() {
@@ -42,9 +46,31 @@ const updateGuestCount = function () {
 
     if (guests.length === 8) { // if the length of guests equals to 8 
         addGuestButton.classList.add("hide"); // then, add hide class to addguestbutton
-        guestInput.classList.add("hide");
+        guestInput.classList.add("hide"); //
         guestInputLabel.classList.add("hide");
         guestFull.classList.remove("hide"); // remove hide class to guestfull variable
     }
 };
 
+const assignItems = function() { // an array of potluck elements 
+    const potluckItems = ["potato salad", "hummus", "coleslaw", "corn dip", "pico de gallo", "cheeseburger sliders", "cucumber salad", "pasta salad", "jamaican rum punch", "honey bun cake", "pitcher margaritas", "deviled eggs"];
+
+
+const allGuests = document.querySelectorAll(".guest-list li"); // selects all list elements inside ul 
+for (let guest of allGuests) { // loops over allGuests using guest
+    let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length); //produce a number b/t 0 and potluckItems to get an element from array
+    let randomPotluckItem = potluckItems[randomPotluckIndex];
+    let listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+    assignedItems.append(listItem);
+    potluckItems.splice(randomPotluckIndex, 1);
+
+    }
+};
+assignButton.addEventListener("click", function () {
+    assignItems();
+
+    
+    assignButton.disabled = true;
+ 
+});
